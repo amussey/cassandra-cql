b046673f46f7d510e09e9bd2be8a3d855e936f47
Andrew Mussey <MusseyAM@vt.edu>
2013-10-11 15:30:16 -0400

    Added a parameter to make the entire execution silent.

--- cassandra_cql/__init__.py
+++ cassandra_cql/__init__.py
@@ -35,8 +35,13 @@ import sys
 import thrift
 from cassandra_cql.bcolors import bcolors
 
-def cql_execute(host, port, filename, force):
-    connection = cql.connect(host=host, port=port, cql_version='3.0.0')
+def cql_execute(host, port, filename, force, silent=False):
+    try:
+        connection = cql.connect(host=host, port=port, cql_version='3.0.0')
+    except thrift.transport.TTransport.TTransportException, e:
+        if not silent:
+            print 'Execution failed: {e}'.format(e=e)
+        return False
     cursor = connection.cursor()
 
     cql_file = open(filename)
@@ -45,18 +50,24 @@ def cql_execute(host, port, filename, force):
 
     for cql_command in cql_command_list:
         if cql_command.replace('\n', ''):
-            print '\n{command}'.format(command=cql_command.strip('\n'))
+            if not silent:
+                print '\n{command}'.format(command=cql_command.strip('\n'))
 
             try:
                 cursor.execute('{command};'.format(command=cql_command.replace('\n', ' ')))
-                print '{color_start}Success!{color_end}'.format(color_start=bcolors.OKGREEN, color_end=bcolors.ENDC)
+                if not silent:
+                    print '{color_start}Success!{color_end}'.format(color_start=bcolors.OKGREEN, color_end=bcolors.ENDC)
             except cql.ProgrammingError as e:
-                print '{color_start}{error}{color_end}'.format(error=e, color_start=bcolors.FAIL, color_end=bcolors.ENDC)
+                if not silent:
+                    print '{color_start}{error}{color_end}'.format(error=e, color_start=bcolors.FAIL, color_end=bcolors.ENDC)
                 if not force:
-                    print '\n{color_start}Execution of script {filename} failed!\nSee the error message above for details.{color_end}'.format(color_start=bcolors.FAIL,
-                                                                                                    color_end=bcolors.ENDC,
-                                                                                                    filename=filename)
-                    sys.exit(-1)
-    print '\n{color_start}Execution of script {filename} complete!{color_end}'.format(color_start=bcolors.OKBLUE,
-                                                                                      color_end=bcolors.ENDC,
-                                                                                      filename=filename)
+                    if not silent:
+                        print '\n{color_start}Execution of script {filename} failed!\nSee the error message above for details.{color_end}'.format(color_start=bcolors.FAIL,
+                                                                                                        color_end=bcolors.ENDC,
+                                                                                                        filename=filename)
+                    return False
+    if not silent:
+        print '\n{color_start}Execution of script {filename} complete!{color_end}'.format(color_start=bcolors.OKBLUE,
+                                                                                          color_end=bcolors.ENDC,
+                                                                                          filename=filename)
+
